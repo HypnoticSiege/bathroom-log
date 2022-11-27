28fe5aa2edfad822c5645cc17eb24177ff00d12a
HypnoticSiege <luis@hypnoticsiege.net>
2022-11-27 16:04:56 -0500

    db(select where clause)

--- src/utils/db/functions/select.ts
+++ src/utils/db/functions/select.ts
@@ -1,13 +1,22 @@
 import supabase from "../../../supabase";
 
 const select = async (table: string, columns: string, where?: string) => {
-    const { data, error } = await supabase
-        .from(table)
-        .select(columns);
-    
-    if (error) console.log(error);
-    console.log(data);
-    return data;
+    if (!where) {
+        let { data, error } = await supabase
+            .from(table)
+            .select(columns);
+        
+        if (error) console.log(error);
+        return data;
+    } else {
+        let { data, error } = await supabase
+            .from(table)
+            .select(columns)
+            .eq('id', where);
+        
+        if (error) console.log(error);
+        return data;
+    }
 };
 
 export default select;
