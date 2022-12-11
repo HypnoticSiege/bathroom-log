2047088766ab97c7925b8af948ada51d7103f970
HypnoticSiege <luis@hypnoticsiege.net>
2022-12-10 22:11:56 -0500

    auth(user locals + if state in footer)

--- src/utils/auth/functions/middleware.ts
+++ src/utils/auth/functions/middleware.ts
@@ -3,6 +3,7 @@ import supabase from "../../../supabase";
 const middleware = async (req: any, res: any, next: any) => {
     const user = await supabase.auth.getUser(req.cookies['supabase-auth-token']);
     req.user = user || null;
+    res.locals.user = user || null;
     
     next();
 };
