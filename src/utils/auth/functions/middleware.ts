f9e95412d5c6bff2935b3d3f55d5039d4bfd6d26
HypnoticSiege <luis@hypnoticsiege.net>
2022-12-11 15:26:26 -0500

    Update middleware.ts

--- src/utils/auth/functions/middleware.ts
+++ src/utils/auth/functions/middleware.ts
@@ -2,7 +2,7 @@ import supabase from "../../../supabase";
 
 const middleware = async (req: any, res: any, next: any) => {
     const user = await supabase.auth.getUser(req.cookies['supabase-auth-token']);
-    req.user = user || null;
+    req.user = user.data || null;
     res.locals.user = user || null;
     
     next();
